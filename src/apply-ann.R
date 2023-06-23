b2549de15cf855d0bd82f0ab0d8dd95ad2d9cb8d
ctastad <ctastad@gmail.com>
2023-06-23 10:42:51 -0400

    added inline comment documentation

--- src/apply-ann.R
+++ src/apply-ann.R
@@ -1,19 +1,22 @@
-library(Seurat)
+library(Seurat) # Load the Seurat library
 
-args <- commandArgs(trailingOnly = TRUE)
-out_path <- paste0(args[1], "/")
-obj <- readRDS(args[2])
-obj_name <- tools::file_path_sans_ext(basename(args[2]))
+args <- commandArgs(trailingOnly = TRUE) # Get the command line arguments
+out_path <- paste0(args[1], "/") # Set the output path
+obj <- readRDS(args[2]) # Read the RDS file specified in the second argument
+obj_name <- tools::file_path_sans_ext(basename(args[2])) # Get the name of the RDS file without extension
 
+# Read the predicted labels from a CSV file
 labels <- read.csv(
   paste0(out_path, "predicted_labels.csv"),
   row.names = 1
 )
 
+# Add the predicted labels to the Seurat object
 obj$predicted_labels <- labels$predicted_labels
 obj$over_clustering <- labels$over_clustering
 obj$majority_voting <- labels$majority_voting
 
+# Save the updated Seurat object to an RDS file with a specific name
 saveRDS(
   object = obj,
   file = paste0(
--- src/matrix-convert.R
+++ src/matrix-convert.R
@@ -1,18 +1,18 @@
-library(Seurat)
-library(scrattch.io)
+library(Seurat) # Load the Seurat library
+library(scrattch.io) # Load the scrattch.io library
 
-args <- commandArgs(trailingOnly = TRUE)
-out_path <- paste0(args[1], "/")
-obj <- readRDS(args[2])
+args <- commandArgs(trailingOnly = TRUE) # Get the command line arguments
+out_path <- paste0(args[1], "/") # Set the output path
+obj <- readRDS(args[2]) # Read the RDS file specified in the second argument
 
-# write expression counts matrix
+# Get the expression counts matrix from the Seurat object
 counts <- GetAssayData(
   obj,
   assay = "RNA",
   slot = "counts"
 )
 
-# write counts matrix to file in memory considerate maner
+# Write the counts matrix to a CSV file in a memory-efficient manner
 write_dgCMatrix_csv(
   counts,
   paste0(out_path, "counts.csv"),
